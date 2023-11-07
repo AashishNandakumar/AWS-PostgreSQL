8c0c3345e09bd267ea2ad4ddcbaa1155f4d77147
Aashish <aashishnkumar@gmail.com>
2023-11-07 14:40:26 +0530

    Removed redundant code

--- pg_adaptor/data_harvesters.py
+++ pg_adaptor/data_harvesters.py
@@ -4,35 +4,6 @@ from dotenv import load_dotenv  #? A function to load .env files
 import os  #? To interact with the OS and its functionalities
 from pg_adaptor import connect
 
-'''
-#? Path where you .env file is present
-env_path = 'D:/Coding-Applications/VS Code/VSC Code files/AWS-postgreSQL/.env'
-load_dotenv(dotenv_path=env_path)
-
-
-#? Load the environment variables into local variables 
-hostname = os.getenv('HOSTNAME1')
-port = os.getenv('PORT')
-username = os.getenv('USERNAME1')
-password = os.getenv('PASSWORD')
-database = os.getenv('DATABASE')
-
-
-#? To connect to the database
-def connect1():
-    try:
-        connection = psycopg2.connect(
-            host=hostname,
-            port=port,
-            user=username,
-            password=password,
-            dbname=database
-        )
-        return connection
-    except Exception as e:
-        print("Couldn't establish connection to the database")
-        print(e)
-'''
 
 #! Insert website data here
 def insert_website_data(data):
